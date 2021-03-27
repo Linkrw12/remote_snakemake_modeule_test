6916ee7446031cbe87fa4d712e1e7a1b1927ac88
Linkrw12 <robert_w_link@yahoo.com>
2021-03-26 22:14:18 -0400

    add_things

---
+++ scripts/.ipynb_checkpoints/print-checkpoint.py
@@ -0,0 +1,18 @@
+import argparse
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description='dummy snakemake function',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("-i", default=None,
+                        help="item to be printed")
+    
+    return parser
+
+def main():
+    args = get_parser().parse_args()
+    print(args.i)
+
+if __name__ == '__main__':
+    main()
